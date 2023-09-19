79a39e99218969ed8c7ba4ae0cf1186ca10da89b
Kei Tashima <kei.tashima@ctc-g.co.jp>
2023-09-19 15:01:34 +0900

    ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { Amplify, Auth } from 'aws-amplify';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
 
-function App() {
+// https://ui.docs.amplify.aws/components/authenticator#quick-start
+export default function App() {
+
+  // Cognito から Token を取得
+  // 参考：https://docs.amplify.aws/lib/restapi/authz/q/platform/js/#cognito-user-pools-authorization
+  const [token, setToken] = useState('');
+  const [errorLog, setErrorLog] = useState('');
+  useEffect(() => {
+    Auth.currentSession().then((session) => {
+      if (session) {
+        setToken(session.getIdToken().getJwtToken());
+      } else {
+        setErrorLog('session is undefined.');
+      }
+    }).catch((e) => { setErrorLog(e) });
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Authenticator loginMechanisms={['email']}>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          {errorLog && (<div>{errorLog}</div>)}
+          <div>
+            Token is {token}
+          </div>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
   );
-}
-
-export default App;
+};
